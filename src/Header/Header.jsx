89eb354dc481a29f467713db474a106dfbb417d7
Andrii Pryhodiuk <prigodyuk06@gmail.com>
2022-10-15 19:12:38 +0300

    removed unused module,created input

---
+++ src/Header/Header.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import "./Header.scss";
+export const Header = () => {
+    return <header className="header"></header>;
+};
---
+++ src/Input/Input.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Input.scss";
+
+export const Input = () => {
+    return (
+        <label>
+            Write:
+            <input className="input" name="" />
+        </label>
+    );
+};
