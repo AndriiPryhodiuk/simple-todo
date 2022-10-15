06e848a47c4ca1be1e102c3ce33e185dce148904
Andrii Pryhodiuk <prigodyuk06@gmail.com>
2022-10-15 21:08:37 +0300

    added Content,Header components

---
+++ src/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.scss";
+
+export const Header = () => {
+    return (
+        <header className="header">
+            <h1>TODO APP</h1>
+        </header>
+    );
+};
